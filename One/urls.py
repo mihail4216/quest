f5f95d8d81d380269ff4bb616761c135faf89424
Mihail <79084786093@mail.ru>
2016-12-20 20:38:28 +0400

    version start

---
+++ One/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from One.views import *
+
+
+
+
+urlpatterns = [
+    url(r'^/',MainView.as_view(),name='main'),
+    # url(r'^logout/', include()),
+
+]
---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from accounts.views import *
+
+urlpatterns = [
+    url(r'^login/',LoginView.as_view(),name='login'),
+    # url(r'^logout/', include()),
+
+]
