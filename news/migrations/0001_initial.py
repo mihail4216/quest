f44cdbe259370b608fdf6d53715523add10f84c1
Mihail <79084786093@mail.ru>
2017-01-11 19:13:33 +0400

    uge norm site

--- news/migrations/0001_initial.py
+++ news/migrations/0001_initial.py
@@ -13,13 +13,27 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Comments',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=40)),
+                ('text', models.TextField()),
+                ('avtor', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+        ),
+        migrations.CreateModel(
             name='News',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(max_length=20)),
+                ('title', models.CharField(unique=True, max_length=20)),
                 ('text', models.TextField()),
                 ('likes', models.IntegerField(default=0)),
-                ('avtor', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+                ('avtor', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
             ],
+        ),
+        migrations.AddField(
+            model_name='comments',
+            name='comments_news',
+            field=models.ForeignKey(blank=True, to='news.News', null=True),
         ),
     ]
