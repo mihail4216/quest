5e4088e34b3af28a35daffe1f14bcafc4819429b
Mihail <79084786093@mail.ru>
2017-03-04 15:54:18 +0400

    add first my js in news

---
+++ static/js/lk.js
@@ -0,0 +1,26 @@
+/**
+ * Created by mihail on 04.03.17.
+ */
+$(document).ready(function () {
+    var photo_s = true;
+    $('#photo').click(function () {
+        // $(this).css('center','center');
+        if (photo_s) {
+            $(this).animate({
+
+                width: $(this).width() * 3,
+                height: $(this).height() * 3
+            },300);
+            photo_s = false;
+        }
+        else {
+            $(this).animate({
+                width: $(this).width() /3,
+                height: $(this).height() / 3
+            });
+            photo_s = true;
+        }
+    })
+
+
+});
